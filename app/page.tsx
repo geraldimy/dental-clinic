ddae109e3a6c1953c87112b8bbc195c4f7371453
geraldimy <geraldimy21@gmail.com>
2026-06-10 13:24:53 +0700

    feat: floating whatsapp

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Testimonials from "@/components/sections/Testimonials";
 import Blog from "@/components/sections/Blog";
 import FAQ from "@/components/sections/FAQ";
 import CTA from "@/components/sections/CTA";
+import WhatsAppFloat from "@/components/ui/WhatsAppFloat";
 
 export default function Home() {
   return (
@@ -24,6 +25,7 @@ export default function Home() {
       <FAQ />
       <CTA />
       <Footer />
+      <WhatsAppFloat />
     </main>
   );
 }
